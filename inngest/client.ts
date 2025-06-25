9ed5ecd292a9670514cfe066e7a3f181b372bce6
S K Sarfaraz <sarfaraz7856sk@gmail.com>
2025-06-25 12:01:16 +0530

    Thirteen commit

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 
+console.log("Inngest event key:", process.env.INNGEST_EVENT_KEY);
+
 export const inngest = new Inngest({
   id: "ai-career-coach-agent",
   eventKey: process.env.INNGEST_EVENT_KEY, // This is required to send events
